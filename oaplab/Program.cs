8836f648a7f2dcd4a113ead5f02956a85ddeeb41
Sokolov21 <80373143+Sokolov21@users.noreply.github.com>
2021-04-14 11:11:07 +0300

    Add files via upload

--- oaplab/Program.cs
+++ oaplab/Program.cs
@@ -1,162 +1,405 @@
-﻿// подключены какие-то библиотеки
+﻿
+            // подключены какие-то библиотеки
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Threading.Tasks;
 
 // namespace такой же как и название проекта
-namespace oapLabaSokolov
+namespace oapLabaSmirnov
 {
-    // про классы мы пока не говорили...
-    class Program
+
+    /*class Program
     {
-         // точка входа в программу
-         static void Main(string[] args)
-        {/*
-            Console.Write("Input number1: ");
-            var number1 = Convert.ToDouble( Console.ReadLine() );
-            Console.Write("Input number2: ");
-            var number2 = Convert.ToDouble( Console.ReadLine() );
-            Console.Write("Input number3: ");
-            var number3 = Convert.ToDouble( Console.ReadLine() );
+        // точка входа в программу
+        static void Main(string[] args)
+        {
+            //ExceptionTest();
+            //ExceptionTest2();
+            //ExceptionTest3();
+            //ExceptionTest4();
+            //ExceptionTest5();
+            //ExceptionTest6();
+            //ExceptionTest7();
+            //ExceptionTest8();
+            //ExceptionTest9();
+            //ExceptionTest10();
+            //ExceptionTest11();
+            ExceptionTest12();
+            //ExceptionTest13();
+            //ExceptionTest14();
+            //ExceptionTest15();
+            //ExceptionTest16();
+            //ExceptionTest17();
+            //ExceptionTest18();
+            //ExceptionTest19();
+            Console.ReadKey();
+        }
 
-            if (number1 >= 0)
-            {
-                number1 = Math.Pow(number1, 2);
-            }
-            else 
-            {
-                number1 = Math.Pow(number1, 4);
-            }
+        static void ExceptionTest()
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
 
-            if (number2 >= 0)
+            foreach (DriveInfo drive in drives)
             {
-                number2 = Math.Pow(number2, 2);
-            }
-            else 
-            {
-                number2 = Math.Pow(number2, 4);
-            }
-
-
-            number3 = (number3 >= 0) ? Math.Pow(number3, 2) : швMath.Pow(number3, 4);
-
-            if (number3 >= 0)
-            {
-                number3 = Math.Pow(number3, 2);
-            }
-            else
-            {
-                number3 = Math.Pow(number3, 4);
-            }
-            Console.WriteLine($"number1 = {number1}");
-            Console.WriteLine($"number2 = {number2}");
-            Console.WriteLine($"number3 = {number3}");
-            
-            */
-            /*
-            Console.Write("Input X1: ");
-            var X1 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Input Y1: ");
-            var Y1 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Input X2: ");
-            var X2 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Input Y2: ");
-            var Y2 = Convert.ToDouble(Console.ReadLine());
-            var Dlina1 = Math.Sqrt(Math.Pow(X1, 2) + Math.Pow(Y1, 2));
-            var Dlina2 = Math.Sqrt(Math.Pow(X2, 2) + Math.Pow(Y2, 2));
-            if (Dlina1 < Dlina2)
-            {
-                Console.WriteLine("первaя точка ближе");
-            }
-            else
-            {
-                Console.WriteLine("вторая точка ближе");
-            }
-           */
-            /*
-            Console.Write("Input Ugol1: ");
-            var Ugol1 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Input Ugol2: ");
-            var Ugol2 = Convert.ToDouble(Console.ReadLine());
-            if((Ugol1 + Ugol2)<180)
-            {
-                Console.WriteLine("треугольник существует");
-                if(Ugol1 == 90 || Ugol2 == 90 || Ugol2 + Ugol1 == 90)
+                Console.WriteLine($"Название: {drive.Name}");
+                Console.WriteLine($"Тип: {drive.DriveType}");
+                if (drive.IsReady)
                 {
-                    Console.WriteLine("треугольник прямоугольный");
+                    Console.WriteLine($"Объем диска: {drive.TotalSize}");
+                    Console.WriteLine($"Свободное пространство: {drive.TotalFreeSpace}");
+                    Console.WriteLine($"Метка: {drive.VolumeLabel}");
                 }
-                else Console.WriteLine("треугольник не прямоугольный");
+                Console.WriteLine();
             }
-            else
-            {
-                Console.WriteLine("треугольник не существует");
-            }
-            */
-            /*
-            Console.Write("Number1: ");
-            var Number1 = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Number2: ");
-            var Number2 = Convert.ToDouble(Console.ReadLine());
-            var PoluSumma = (Number1 + Number2) / 2;
-            var Proizvedenie = 2 * (Number1 * Number2);
-            if (Number1 < Number2)
-            {
-                Number1 = PoluSumma;
-                Number2 = Proizvedenie;
-            }
-            else
-            {
-                Number2 = PoluSumma;
-                Number1 = Proizvedenie;
-            }
-            Console.WriteLine($"number1 = {Number1}");
-            Console.WriteLine($"number2 = {Number2}");
-            */
-            Console.Write("Input X: ");
-            var X = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Input Y: ");
-            var Y = Convert.ToDouble(Console.ReadLine());
-            if(X == 0)
-            {
-                if (Y==0)
-                    Console.WriteLine("является началом координат");
-                else 
-                Console.WriteLine("пренадлежит оси Y");
 
-            }
-            else
+        }
+
+        static void ExceptionTest2()
+        {
+            string dirName = "C:\\";
+
+            if (Directory.Exists(dirName))
             {
-                if(X > 0 && Y > 0)
+                Console.WriteLine("Подкаталоги:");
+                string[] dirs = Directory.GetDirectories(dirName);
+                foreach (string s in dirs)
                 {
-                    Console.WriteLine("пренадлежит первой четверти");
+                    Console.WriteLine(s);
                 }
-                else if (X > 0 && Y < 0)
-                { 
-                    Console.WriteLine("пренадлежит четвертой четверти"); 
-                }
-             if (Y == 0)
+                Console.WriteLine();
+                Console.WriteLine("Файлы:");
+                string[] files = Directory.GetFiles(dirName);
+                foreach (string s in files)
                 {
-                    Console.WriteLine("пренадлежит оси X");
-                }
-             else
-                {
-                    if (X < 0 && Y > 0)
-                    {
-                        Console.WriteLine("пренадлежит второй четверти");
-                    }
-                    else if (X < 0 && Y < 0)
-                    {
-                        Console.WriteLine("пренадлежит третей четверти");
-                    }
-            
+                    Console.WriteLine(s);
                 }
             }
-            Console.Write("Press ENTER to continue...");
+        }
+
+        static void ExceptionTest3()
+        {
+            string path = @"C:\SomeDir";
+            string subpath = @"program\avalon";
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+            dirInfo.CreateSubdirectory(subpath);
+        }
+
+        static void ExceptionTest4()
+        {
+            string dirName = "C:\\Program Files";
+
+            DirectoryInfo dirInfo = new DirectoryInfo(dirName);
+
+            Console.WriteLine($"Название каталога: {dirInfo.Name}");
+            Console.WriteLine($"Полное название каталога: {dirInfo.FullName}");
+            Console.WriteLine($"Время создания каталога: {dirInfo.CreationTime}");
+            Console.WriteLine($"Корневой каталог: {dirInfo.Root}");
+        }
+
+        static void ExceptionTest5()
+        {
+            string dirName = @"C:\SomeFolder";
+
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(dirName);
+                dirInfo.Delete(true);
+                Console.WriteLine("Каталог удален");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        
+        static void ExceptionTest6()
+        {
+            string oldPath = @"C:\SomeFolder";
+            string newPath = @"C:\SomeDir";
+            DirectoryInfo dirInfo = new DirectoryInfo(oldPath);
+            if (dirInfo.Exists && Directory.Exists(newPath) == false)
+            {
+                dirInfo.MoveTo(newPath);
+            }
+        }
+
+        static void ExceptionTest7()
+        {
+            string path = @"C:\Users\User\source\repos\Oap_Lab\фигня.docx";
+            FileInfo fileInf = new FileInfo(path);
+            if (fileInf.Exists)
+            {
+                Console.WriteLine("Имя файла: {0}", fileInf.Name);
+                Console.WriteLine("Время создания: {0}", fileInf.CreationTime);
+                Console.WriteLine("Размер: {0}", fileInf.Length);
+            }
+
+        }
+
+        static void ExceptionTest8() { 
+        string path = @"C:\Users\User\source\repos\Oap_Lab\фигня.docx";
+            FileInfo fileInf = new FileInfo(path);
+        if (fileInf.Exists)
+        {
+        fileInf.Delete();
+        // альтернатива с помощью класса File
+        // File.Delete(path);
+            }
+}
+
+        static void ExceptionTest9()
+        {
+            string path = @"C:\Users\User\source\repos\Oap_Lab\фигня.docx";
+            string newPath = @"C: \Users\User\source\repos\фигня.docx";
+            FileInfo fileInf = new FileInfo(path);
+            if (fileInf.Exists)
+            {
+                fileInf.MoveTo(newPath);
+                // альтернатива с помощью класса File
+                // File.Move(path, newPath);
+            }
+            Console.WriteLine("Файл перемещен");
+        }
+
+        static void ExceptionTest10()
+        {
+            string path = @"C:\И-21";
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            if (!dirInfo.Exists) 
+            {
+                dirInfo.Create();
+            }
+            Console.WriteLine("Введите строку для записи в файл:");
+            string text = Console.ReadLine();
+
+            // запись в файл
+            using (FileStream fstream = new FileStream($"{path}\\note.txt", FileMode.OpenOrCreate))
+            {
+                // преобразуем строку в байты
+                byte[] array = System.Text.Encoding.Default.GetBytes(text);
+                // запись массива байтов в файл
+                fstream.Write(array, 0, array.Length);
+                Console.WriteLine("Текст записан в файл");
+            }
+
+            // чтение из файла
+            using (FileStream fstream = File.OpenRead($"{path}\\note.txt"))
+            {
+                // преобразуем строку в байты
+                byte[] array = new byte[fstream.Length];
+                // считываем данные
+                fstream.Read(array, 0, array.Length);
+                // декодируем байты в строку
+                string textFromFile = System.Text.Encoding.Default.GetString(array);
+                Console.WriteLine($"Текст из файла: {textFromFile}");
+            }
+
             Console.ReadLine();
         }
- }
+
+        static void ExceptionTest11()
+        {
+            string text = "hello world";
+
+            // запись в файл
+            using (FileStream fstream = new FileStream(@"D:\\note.dat", FileMode.OpenOrCreate))
+            {
+                // преобразуем строку в байты
+                byte[] input = Encoding.Default.GetBytes(text);
+                // запись массива байтов в файл
+                fstream.Write(input, 0, input.Length);
+                Console.WriteLine("Текст записан в файл");
+
+                // перемещаем указатель в конец файла, до конца файла- пять байт
+                fstream.Seek(-5, SeekOrigin.End); // минус 5 символов с конца потока
+
+                // считываем четыре символов с текущей позиции
+                byte[] output = new byte[4];
+                fstream.Read(output, 0, output.Length);
+                // декодируем байты в строку
+                string textFromFile = Encoding.Default.GetString(output);
+                Console.WriteLine($"Текст из файла: {textFromFile}"); // worl
+
+                // заменим в файле слово world на слово house
+                string replaceText = "house";
+                fstream.Seek(-5, SeekOrigin.End); // минус 5 символов с конца потока
+                input = Encoding.Default.GetBytes(replaceText);
+                fstream.Write(input, 0, input.Length);
+
+                // считываем весь файл
+                // возвращаем указатель в начало файла
+                fstream.Seek(0, SeekOrigin.Begin);
+                output = new byte[fstream.Length];
+                fstream.Read(output, 0, output.Length);
+                // декодируем байты в строку
+                textFromFile = Encoding.Default.GetString(output);
+                Console.WriteLine($"Текст из файла: {textFromFile}"); // hello house
+            }
+            Console.Read();
+        }
+
+        static void ExceptionTest12()
+        {
+            string writePath = @"C:\И-21\hta.txt";
+
+            string text = "Привет мир!\nПока мир...";
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(writePath, false, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine(text);
+                }
+
+                using (StreamWriter sw = new StreamWriter(writePath, true, System.Text.Encoding.Default))
+                {
+                    sw.WriteLine("Дозапись");
+                    sw.Write(4.5);
+                }
+                Console.WriteLine("Запись выполнена");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        
+        static void ExceptionTest13()
+        {
+            string path = @"C:\SomeDir\hta.txt";
+
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            // асинхронное чтение
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+            {
+                string line;
+                while ((line = await sr.ReadLineAsync()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        } 
+            
+    }*/
+    /*struct State
+    {
+        public string name;
+        public string capital;
+        public int area;
+        public double people;
+
+        public State(string n, string c, int a, double p)
+        {
+            name = n;
+            capital = c;
+            people = p;
+            area = a;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            State[] states = new State[2];
+            states[0] = new State("Германия", "Берлин", 357168, 80.8);
+            states[1] = new State("Франция", "Париж", 640679, 64.7);
+
+            string path = @"C:\SomeDir\states.dat";
+
+            try
+            {
+                // создаем объект BinaryWriter
+                using (BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate)))
+                {
+                    // записываем в файл значение каждого поля структуры
+                    foreach (State s in states)
+                    {
+                        writer.Write(s.name);
+                        writer.Write(s.capital);
+                        writer.Write(s.area);
+                        writer.Write(s.people);
+                    }
+                }
+                // создаем объект BinaryReader
+                using (BinaryReader reader = new BinaryReader(File.Open(path, FileMode.Open)))
+                {
+                    // пока не достигнут конец файла
+                    // считываем каждое значение из файла
+                    while (reader.PeekChar() > -1)
+                    {
+                        string name = reader.ReadString();
+                        string capital = reader.ReadString();
+                        int area = reader.ReadInt32();
+                        double population = reader.ReadDouble();
+
+                        Console.WriteLine("Страна: {0}  столица: {1}  площадь {2} кв. км   численность населения: {3} млн. чел.",
+                            name, capital, area, population);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }*/
+    [Serializable]
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
     }
 
-            
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // объект для сериализации
+            Person person = new Person("Tom", 29);
+            Console.WriteLine("Объект создан");
+
+            // создаем объект BinaryFormatter
+            BinaryFormatter formatter = new BinaryFormatter();
+            // получаем поток, куда будем записывать сериализованный объект
+            using (FileStream fs = new FileStream("people.dat", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, person);
+
+                Console.WriteLine("Объект сериализован");
+            }
+
+            // десериализация из файла people.dat
+            using (FileStream fs = new FileStream("people.dat", FileMode.OpenOrCreate))
+            {
+                Person newPerson = (Person)formatter.Deserialize(fs);
+
+                Console.WriteLine("Объект десериализован");
+                Console.WriteLine($"Имя: {newPerson.Name} --- Возраст: {newPerson.Age}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
